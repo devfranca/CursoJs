3e3f88fe5d797a5c8b28ec20e8f67de9b559cf07
francadev <yangabrielfranca15@gmail.com>
2023-08-30 22:47:44 -0300

    aulas

---
+++ aula10/index.js
@@ -0,0 +1,24 @@
+/** 
+ * Aritimeticos
+ * + adicao/concatenação
+ * - subtracao
+ *  * multiplicacao
+ *  / divisao
+ *  ** potenciação
+ *  % resto da divisao
+ * ++ incremento
+ *  -- decremento
+ * 
+ */
+
+// Nan- not a number
+// parsetInt(inteiro), converde um dado do tipo string para um numeoro inteiro
+
+// parsetFLoat(decimal), faz a mesma coisa porem com numeros decimais ( flaot).
+
+//Number converte a string dentro dos pareteses em um numero converte indepedente do tipo dele.
+
+const num1 = 2;
+const num2 = 10;
+
+console.log(num1 ** num2);
---
+++ aula11/js/script.js
@@ -0,0 +1,8 @@
+let num1 = prompt('digite um numero');
+let num2 = prompt('digite outro numero');
+
+num1 = Number(num1);
+num2 = Number(num2);
+const resultado = num1 + num2;
+
+alert(`seu resultado foi : ${resultado}`)
