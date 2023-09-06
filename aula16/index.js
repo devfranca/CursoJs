40c24aeaae61c55db813678dc71b5c30d452f0d5
francadev <yangabrielfranca15@gmail.com>
2023-09-05 22:53:57 -0300

    .

---
+++ aula16/index.js
@@ -0,0 +1,6 @@
+//os arrays tambem sao indexados porem de uma forma diferente.
+
+//               0       1          2          3
+const alunos = ['yan','carlos','edenilso', 'mbappe']; // ou seja cada array equivale a um numero index
+
+console.log(alunos)
