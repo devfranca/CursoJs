0cf73c98403902435324ce2073e9f08951a7c91f
francadev <yangabrielfranca15@gmail.com>
2023-09-07 20:09:07 -0300

    aula 17

--- aula16/index.js
+++ aula16/index.js
@@ -6,45 +6,45 @@ alunos = 123;
 console.log(typeof alunos);
 console.log( alunos instanceof Array);
 
-// alunos.push('melissa'); 
-// alunos.push('mel');
-// console.log(alunos.slice(0,3))
+alunos.push('melissa'); 
+alunos.push('mel');
+console.log(alunos.slice(0,3))
 //deste forma voce pode deletar uma array sem alterar os indices de dentro, porem o array ficara com um espaço vazio.
-// delete alunos[1];
-// console.log(alunos);
+delete alunos[1];
+console.log(alunos);
 
 // funcao POP remove o ultimo array.
-// alunos.pop();
+alunos.pop();
 
 //funcao SHIFT remove o primeiro aray, assim mudando todos os indices do array
-// alunos.shift()
+alunos.shift()
 
 //tambem podemos salvar esse array excluido em uma variavel.
-// const arrayRemovido = alunos.shift()
-// console.log(arrayRemovido) // mostra o array que foi removido
-// console.log(alunos) // mostra os arrays que restaram
+const arrayRemovido = alunos.shift()
+console.log(arrayRemovido) // mostra o array que foi removido
+console.log(alunos) // mostra os arrays que restaram
 
 
 
 
 // UNSHIFT adiciona no comeco dos array, assim alterando todo o indice mostrado acima, neste caso o indice 0 vai ser o choice pois foi o ultimo a ser adicinado, caso adicionasse mais um choice seria passado para frente e seria o indice 1 e assim por diante
-// alunos.unshift('cesar');
-// alunos.unshift('choice');
-// console.log(alunos)
+alunos.unshift('cesar');
+alunos.unshift('choice');
+console.log(alunos)
 
 
 // funcao PUSH neste caso adiciona um array apos o ultimo array.
-// alunos.push('sant');
-// alunos.push('armando');
+alunos.push('sant');
+alunos.push('armando');
 
-// alunos[alunos.length] = 'sant' // tambem adiciona um array no final
-// alunos[alunos.length] = 'armando'
-// console.log(alunos)
+alunos[alunos.length] = 'sant' // tambem adiciona um array no final
+alunos[alunos.length] = 'armando'
+console.log(alunos)
 
-// alunos[0] = 'ph' // aqui ele edita o indice 0 
-// alunos[4] = 'carlin' // como nao tem o indice 4 ele cria.
-// console.log(alunos)
+alunos[0] = 'ph' // aqui ele edita o indice 0 
+alunos[4] = 'carlin' // como nao tem o indice 4 ele cria.
+console.log(alunos)
 
-// console.log(alunos);
-// console.log(alunos[0]);
-// console.log(alunos[3]);
+console.log(alunos);
+console.log(alunos[0]);
+console.log(alunos[3]);
---
+++ aula17/index.js
@@ -0,0 +1,36 @@
+// function saudacao(nome){
+//     return `Bom dia, ${nome}!`; // faz a funcao retornar algo, originalmente uma funcao criada nao retorna nada (undefined)
+// }
+
+
+// const variavel =  saudacao('yan');
+// console.log(variavel) 
+
+
+            //se nenhum valor for enviado a engine assumira esse valor como padrao
+function soma(x = 1, y = 1){
+    const resultado = x + y;
+    return resultado;
+}
+
+// const resultado = soma(3,3);
+// console.log(resultado);
+
+// console.log(soma(2,3));
+// console.log(soma(2,4));
+// console.log(soma(2,5));
+
+//estilo funcao normais
+const raiz = function(numero){
+    return numero ** 0.5;
+};
+console.log(raiz(9))
+console.log(raiz(16))
+console.log(raiz(25))
+
+// arrow function, caso a linha de codigo da funcao for curta ou mais simples é possivel fazer ela da maneira abaixo.
+const divisao = n => n / 2;
+console.log(divisao(10))
+console.log(divisao(20))
+console.log(divisao(30))
+
