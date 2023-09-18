82eac400ffda3c0e978c6fb8adbd255b2577444d
francadev <yangabrielfranca15@gmail.com>
2023-09-18 18:21:09 -0300

    aula20

--- aula20/js/script.js
+++ aula20/js/script.js
@@ -2,18 +2,28 @@ function meuEscopo () {
     const form = document.querySelector('.formulario');
     const resultado = document.querySelector('.resultado');
     const pessoas = [];
-    function recebeEventoForm (evento){
 
+
+    function recebeEventoForm (evento){
     evento.preventDefault();
     const nome = form.querySelector('.nome');
     const sobrenome = form.querySelector('.sobrenome');
     const peso = form.querySelector('.peso');
     const altura = form.querySelector('.altura');
-    
-    
-    
-    }
-    form.addEventListener('submit', recebeEventoForm)
+
+    pessoas.push({
+    nome:nome.value,
+    sobrenome:sobrenome.value,
+    peso:peso.value,
+    altura:altura.value
+});
+    console.log(pessoas);
+
+    resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ${peso.value} ${altura.value}</p>`;
+
+}
+form.addEventListener('submit', recebeEventoForm);
+
 };
 
 meuEscopo();
