5e29cf587dc7d9308216cc8100fc925192d72a6a
francadev <yangabrielfranca15@gmail.com>
2023-09-19 23:01:44 -0300

    .

---
+++ aula21/index.js
@@ -0,0 +1,23 @@
+/* 
+OPERADORES DE COMPARAÇÃO
+
+> MAIOR QUE 
+
+>= MAIOR QUE OU IGUAL A
+
+< MENOR QUE
+
+<= MENOR QUE OU IGUAL A 
+
+== IGUALDADE (VALOR) *********
+
+=== IGUALDADE ESTRITA ( VALOR E TIPO) 
+
+!= DIFERENTE ********
+
+!== DIFERENTE ESTRITO (VALOR E TIPO)
+*/
+const num1= 10;
+const num2 = 10;
+const comparacao = num1 == num2 ;
+console.log(comparacao)
