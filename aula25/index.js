9675bcfe9598b625c00a71e65e2064c4ab9c017e
francadev <yangabrielfranca15@gmail.com>
2023-09-29 21:25:01 -0300

    aula 25

--- aula25/index.js
+++ aula25/index.js
@@ -4,9 +4,9 @@ if(numero >= 0 && numero <= 5 ){
     console.log('O numero esta entre 0 e 5')
 }else if(numero >= 6 && numero <=8){
     console.log('O numero esta entre 6 e 8')
-
+}
     // o primeiro elseif sera executado assim, ignorando todos os outros blocos sendo eles verdadeiros ou nao, EXEMPLO:
-// }else if(1===1){
+// else if(1===1){
 //     console.log("LITERAL") //verdadeiro
 // }
 else if(numero >= 9 && numero <=11){
