90dd34c357b67d6cea62c90c35e3359b3bec2a54
francadev <yangabrielfranca15@gmail.com>
2023-11-04 00:24:16 -0300

    aula 29

---
+++ aula29/index.js
@@ -0,0 +1,35 @@
+const data = new Date();
+const diaSemana = data.getDay();
+function getWeekDay(diaSemana){
+    
+    let diaSemanaTexto;
+    switch(diaSemana){
+        case 0 :
+            diaSemanaTexto = 'Domingo';
+            break;
+        case 1 : 
+            diaSemanaTexto = 'Segunda';
+            break;
+        case 2 : 
+            diaSemanaTexto = 'Terça';
+            break;
+        case 3 : 
+            diaSemanaTexto = 'Quarta';
+            break;
+        case 4 : 
+            diaSemanaTexto = 'Quinta';
+            break;
+   
+            diaSemanaTexto = 'Sexta';
+            break;
+        case 6 : 
+            diaSemanaTexto = 'Sabado';
+            break;
+        default : 
+            diaSemanaTexto = 'Insira uma data'
+    }
+
+    return diaSemanaTexto;
+}
+const diaSemanaTexto = getWeekDay(diaSemana)
+console.log(diaSemana, diaSemanaTexto)
