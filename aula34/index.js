9d263c6b4984f7664b71113bc0be7b72dd6ac273
francadev <yangabrielfranca15@gmail.com>
2023-11-08 20:46:14 -0300

    aula 34

---
+++ aula34/index.js
@@ -0,0 +1,20 @@
+// console.log('Linha 0');
+// console.log('Linha 1');
+// console.log('Linha 2');
+// console.log('Linha 3');
+// console.log('Linha 4');
+// console.log('Linha 6');
+
+
+// i - index, por isso é padrao progamadores utilizarem o i nesse tipo de situação
+// for (let i = 0; i <= 10 ; i++){
+//    const par = i % 2 === 0 ? 'par' : 'impar'
+//     console.log(i,par)
+// }
+
+const frutas =['Maça', 'Pera', 'Uva','Banana', 'Jabuticaba','Abacaxi','Acerola'];
+
+for (let i = 0; i < frutas.length; i++){
+    console.log(`indice ${i}`,frutas[i]);
+
+}
