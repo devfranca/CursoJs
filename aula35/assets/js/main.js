5f6ed14a393c4107746297601388eb4ae74db2b7
francadev <yangabrielfranca15@gmail.com>
2023-11-09 23:16:00 -0300

    aula 35

---
+++ aula35/assets/js/main.js
@@ -0,0 +1,20 @@
+const elementos = [
+    { tag: 'p', texto: 'frase 1' },
+    { tag: 'div', texto: 'frase 2' },
+    { tag: 'footer', texto: 'frase 3' },
+    { tag: 'section', texto: 'frase 4' },
+];
+const texto = document.querySelector('.container');
+
+const div = document.createElement('div')
+// div.appendChild(texto);
+
+for (let i = 0; i < elementos.length; i++) {
+    let {tag,texto} = elementos[i];
+    let criarTag =  document.createElement(tag);
+    criarTag.innerHTML = texto;
+    div.appendChild(criarTag)
+};
+
+
+texto.appendChild(div)
