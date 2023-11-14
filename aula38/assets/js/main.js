fb5ad959a96cc1c5baf7cb85fca6f947b72e101b
francadev <yangabrielfranca15@gmail.com>
2023-11-13 22:31:28 -0300

    aula 38

---
+++ aula38/assets/js/main.js
@@ -0,0 +1,12 @@
+const paragrafos = document.querySelector('.paragrafos');
+const ps = paragrafos.querySelectorAll('p');
+
+const estilos = getComputedStyle(document.body);
+const CorDeFundo = estilos.backgroundColor
+
+// console.log(CorDeFundo)
+
+for ( let p of ps){
+    p.style.backgroundColor = CorDeFundo;
+    p.style.color = 'white';
+}
